3702fc6d02f80830491065b8a389c383766ea84e
Okonkwo Afam <afamsimon@gmail.com>
2025-01-29 18:23:22 +0100

    shape counting game

--- src/pages/task2_instruction.js
+++ src/pages/task2_instruction.js
@@ -8,14 +8,14 @@ const Task2InstructionPage = () => {
     <Layout pageTitle="Shape Counting Game">
       <h3>Shape Counting Game</h3>
       
-      <p>Please <em><strong>DO NOT</strong></em> read the numbers out loud during the task <br/>
+      <p>Now you will <em><strong>play a Shape Counting</strong></em> game <br/>
       but try to remember them in your head.<br/></p>
 
-      <p>For this task, you will be given shape to count.<br/></p>
-      <p>The shapes will appear one after the other,<br/></p>
-      <p>the list to be remembered will become a bit longer<br/></p>
-
-      <p>Click <Link to='/start_task2'>Next to kick-start task2</Link> to continue.<br/>.<br/>
+      <p>You will see a series of shapes(squares, triangles, and circles) one at a time.<br/></p>
+      <p>Your job is to keep a MENTAL count for each type of shape.<br/></p>
+      <p>You can keep count out loud  or in your head, but please <em>DO NOT</em> use your fingers or a pencil/pen and paper to count<br/></p>
+      <p>After you see each shape series, you will be asked how many of each type of shape you saw.<br/></p>
+      <p>Click <Link to='/start_task2'>Next</Link> to continue to task 2 page.<br/><br/>
       </p>
       
       </Layout>
